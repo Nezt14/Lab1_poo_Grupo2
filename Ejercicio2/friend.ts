e0907099836e4c2638ffd8bf63e33cbe93fc353c
Nezt14 <mnestor1452@gmail.com>
2025-02-27 20:55:00 -0600

    a

--- Ejercicio2/friend.ts
+++ Ejercicio2/friend.ts
@@ -1,23 +1,40 @@
 import { rl } from "./readline";
 
 export class Friends {
-    constructor(public id :number, public name:string ){ 
+    constructor(public id: number, public name: string) {
     }
-    public showFriends():string{
+    public showFriends(): string {
         return `Sus amigos actuales son: \n ${friendsList}  `
     }
 }
 
-const friendsList : Friends[]=[
-    new Friends(1,`Manuel`),
-    new Friends(2,`Cartonero`),
+export const friendsList: Friends[] = [
+    new Friends(1, `Manuel`),
+    new Friends(2, `Cartonero`),
 ];
 
-const indice = friendsList.findIndex(Friends =>Friends.id === 2)
-const indice2 = friendsList.findIndex(Friends =>Friends.id === 1)
-console.log(`amigo: ${friendsList[indice].name}`)
+const amigo1 = friendsList.findIndex(Friends => Friends.id === 1)
+const amigo2 = friendsList.findIndex(Friends => Friends.id === 2)
 
-console.log(friendsList[indice].name, friendsList[indice2].name)
-async function findFriend(){// esto para compartir con los amigos
-const opcion = Number(await rl.question(`h`))
+const amigo3 = friendsList.findIndex(Friends => Friends.id === 3)
+
+console.log(`amigo: ${friendsList[amigo2].name}`)
+
+console.log(friendsList[amigo1].name, friendsList[amigo2].name)
+export async function findFriend() {// esto para compartir con los amigos
+    const opcion = Number(await rl.question(`con quien desea compartir esta publicacion? \n 1. ${amigo1} \n 2. ${amigo2} `))
+    switch (opcion) {
+        case 1:
+            console.log(`su (aqui la publicacion que se selecciono) fue compartida con ${amigo1} `)
+            break;
+        case 2:
+            console.log(`su (aqui la publicacion que se selecciono) fue compartida con ${amigo2} `)
+            break;
+        case 3:
+            console.log(`su (aqui la publicacion que se selecciono) fue compartida con ${amigo3} `)
+            break;
+
+        default:
+            break;
+    }
 }
